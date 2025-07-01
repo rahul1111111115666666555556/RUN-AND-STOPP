24120e59fdd3f44c1f6b6fb708fbf1c8bd374dab
rahul1111111115666666555556 <rahultiwari22272828282828@gmail.com>
2025-07-01 21:57:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,8 +25,6 @@ def index():
 
     <meta charset="UTF-8">
 
-    <title>🦋𝗠𝗥 𝗗𝗘𝗩𝗜𝗟 𝗣𝗢𝗦𝗧-𝗖𝗢𝗠𝗠𝗘𝗡𝗧𝗦-𝗧𝗢𝗢𝗟🦋</title>
-
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
